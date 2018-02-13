6b1f3dedabd1d5d24b24ee3ed002646c1cd60a9f
Georgia Millard <georgiamillardccm@gmail.com>
2018-02-13 10:22:33 +0000

    updated version with capitalization and punctuation error adding to screen

--- script.js
+++ script.js
@@ -2,28 +2,45 @@ $(document).ready(function(){
    
     function make_ipsom(){
         //----------------------------words
-        var words=[' pika pika', ' pikachu', ' thunderbolt', ' trainer',' battle',' abra',' ash', ' ace trainer', ' pokeball', ' charmander', ' char', ' squirtle', ' bulbasaur', ' i choose you',' onyx', ' jessie james', ' meaowth', ' team rocket', ' blasting of again',' pokedex', ' gameboy colour', ' sycther', ' mew-two', ' mew', ' a wild ', ];
+        var words=['pika pika', 'pikachu', 'thunderbolt', 'trainer','battle','abra', ' ash', 'ace trainer', 'pokeball', 'charmander', 'char', 'squirtle', 'bulbasaur', 'i choose you','onyx', 'jessie james', 'meaowth', 'team rocket', 'blasting of again','pokedex',
+        'gameboy colour', 'sycther', 'mew-two', 'mew', 'a wild ', ];
         //------------------------------ turning this into a function
         var number_of_paragraphs = $('#number_of_paragraphs').val();
+        number_of_paragraphs = parseInt(number_of_paragraphs);
         //---------------------------------------varaibales
-        var paragraph='';
-        var limit = words.length;
-        var a = 0;
-        var text='';
-    //--------------------------------- making paragraphs
-    while (a < number_of_paragraphs){
-        for(i=0; i<limit; i++){
-            var word = Math.floor(Math.random() * limit);
-            paragraph += words[word];
-        }
-        paragraph+= '.\n\n\n';
-        text += paragraph;
-        paragraph='';
-        a++;
+        var words_in_para = 40;
+        var output = [];
+        //flag for loop
+        var flag = 0;
+
+        while (flag < number_of_paragraphs){
+            for(var i = 0; i< (words_in_para+1); i++){
+                //adding variables to choose random word.
+                var choice = Math.floor((Math.random() * words.length) + 1);
+                var word = words[choice];
+                if(i == 0){ // capitalises
+                    output.push(capitalize(word));
+                }
+                else if(i == words_in_para){
+                    output.push(word+'.');
+                }
+                else{
+                    output.push(word);
+                }
+            }
+            output.push('</br>');
+            flag ++;
+        }// end of while loop
+       var output_string = output.join(' ');
+       $('#text_area').append(output_string);
     }
-    $('#text_area').append("<p>"+text+"</p>");
-    };
-    
+      
+      // string is a keyword for a data type, as this uses methods and objects we must specify this.
+    function capitalize(string){
+        return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+
     //---------------- only make ipsom when told
     $('#submit').on('click', function(){
     $('#text_area > p').remove();
